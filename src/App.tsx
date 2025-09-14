8e63f43898028303635d54c942fbeb841fafcf79
Treminio Dev <eduardotreminio10@gmail.com>
2025-09-14 16:56:10 -0600

    add animation to navbar, add animation shine text

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <header className="px-6 md:px-20 fixed top-0 left-0 right-0 z-50 bg-transparent shadow-md">
+      <header className="px-6 md:px-20 fixed top-0 left-0 right-0 z-50 bg-trasparent backdrop-blur-md shadow-md ">
         <nav
           className="flex justify-between items-center p-4 fade-intwo"
           aria-label="Menú principal"
@@ -18,31 +18,40 @@ function App() {
           {/* LOGO */}
           <a
             href="/"
-            className="text-xl font-sans font-bold"
+            className="text-2xl font-sans font-bold text-[#cdcdcd] hover:text-blue-500"
             aria-label="Ir al inicio"
           >
-            Jorge Treminio
+            JTDev
           </a>
 
           {/* ENLACES DESKTOP */}
           <div className="hidden md:flex gap-5 text-lg font-semibold">
-            <a href="#inicio" className="hover:text-blue-500">
+            <a href="#inicio" className="hover:text-blue-500 underline-animate">
               Inicio
             </a>
-            <a href="#tecnologias" className="hover:text-blue-500">
+            <a
+              href="#tecnologias"
+              className="hover:text-blue-500 underline-animate"
+            >
               Tecnologías
             </a>
-            <a href="#proyectos" className="hover:text-blue-500">
+            <a
+              href="#proyectos"
+              className="hover:text-blue-500 underline-animate"
+            >
               Proyectos
             </a>
-            <a href="#experiencia" className="hover:text-blue-500">
+            <a
+              href="#experiencia"
+              className="hover:text-blue-500 underline-animate"
+            >
               Experiencia
             </a>
           </div>
 
           {/* BOTÓN HAMBURGUESA */}
           <button
-            className="md:hidden text-2xl focus:outline-none"
+            className="md:hidden text-6xl focus:outline-none"
             onClick={() => setIsOpen(!isOpen)}
             aria-label="Abrir menú"
           >
@@ -51,7 +60,7 @@ function App() {
 
           {/* MENÚ MÓVIL */}
           {isOpen && (
-            <div className="absolute top-full left-0 w-full gradient-burguernaver text-white flex flex-col items-center py-4 space-y-3 md:hidden">
+            <div className="absolute top-full left-0 w-full h-screen gradient-burguernaver text-white flex flex-col items-center pt-10 space-y-20 text-4xl font-black md:hidden">
               <a
                 href="#inicio"
                 className="hover:text-blue-500"
@@ -84,13 +93,14 @@ function App() {
           )}
         </nav>
       </header>
-      <main className="min-h-screen pt-0 grid grid-cols-1 md:grid-cols-2 items-center justify-items-center gap-10 px-6 md:px-20">
+
+      <main className="min-h-screen pt-0 grid grid-cols-1 lg:grid-cols-2 items-center justify-items-center gap-10 px-12">
         {/* Contenedor Flex para avatar y texto */}
-        <div className="flex flex-col col-span-2 md:flex-row items-center md:items-start justify-center w-full">
+        <div className="flex flex-col col-span-2 lg:flex-row items-center lg:items-start justify-center w-full">
           {/* AVATAR */}
-          <aside className="mb-6 md:mb-0 md:mr-10 order-first md:order-last flex-shrink-0">
+          <aside className="mb-6 lg:mb-0 lg:mr-10 order-first lg:order-last flex-shrink-0">
             <div className="relative w-64 h-64 rounded-full">
-              <div className="absolute inset-0 rounded-full bg-gradient-to-r from-blue-500 to-blue-800 blur-lg float-fade"></div>
+              <div className="absolute inset-0 rounded-full bg-[#1655b4] blur-lg float-fade"></div>
               <img
                 src={george}
                 alt="Foto de Jorge Treminio, Desarrollador Full Stack"
@@ -100,27 +110,28 @@ function App() {
           </aside>
 
           {/* TEXTO DE PRESENTACIÓN */}
-          <section className="text-white mt-6 md:mt-0 max-w-xl scroll-mt-70 order-last md:order-first flex flex-col">
-            <h1 className="text-4xl font-black slide-right">
+          <section className="text-white mt-6 lg:mt-0 max-w-xl scroll-mt-70 order-last lg:order-first flex flex-col">
+            <h1 className="text-4xl font-black slide-right text-center lg:text-left">
               ¡Hola! ✌️ Soy{" "}
-              <span className="font-black fade-in bg-gradient-to-r from-blue-400 to-blue-600 bg-clip-text text-transparent">
+              <span className="font-black fade-in bg-gradient-to-r from-blue-400 to-blue-600 bg-clip-text text-transparent ">
                 Jorge Treminio
               </span>
             </h1>
 
-            <h2 className="mt-3 font-bold text-2xl fade-intwo">
+            <h2 className="shine-text  font-bold text-2xl text-center lg:text-left">
               Desarrollador Full Stack
             </h2>
 
-            <p className="mt-2 fade-inthree leading-relaxed">
-              Desarrollo aplicaciones web modernas utilizando{" "}
-              <strong>React</strong> en el frontend y <strong>.NET</strong> en
-              el backend. Me apasiona crear interfaces atractivas, rápidas y
-              optimizadas para SEO.
+            <p className="mt-2 fade-inthree leading-relaxed text-center lg:text-start lg:pr-20">
+              Especializado en la creación de aplicaciones web dinámicas y
+              escalables. Con dominio de tecnologías modernas como{" "}
+              <strong>React</strong> y <strong>.Net </strong>. Apasionado por el
+              aprendizaje continuo y la construcción de soluciones eficientes
+              que aporten valor real a los usuarios.
             </p>
 
             {/* BOTÓN + REDES */}
-            <div className="flex flex-row items-center space-x-3 mt-6 fade-infour">
+            <div className="flex flex-row items-center justify-center lg:justify-start space-x-3 mt-6 fade-infour">
               <Button
                 className="px-6 py-2"
                 color="primary"
